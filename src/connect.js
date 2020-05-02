6b9911fc6ede74f3c65b7352ed7ed58c1529bf64
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2020-05-02 14:30:03 +0900

    Some fix

--- src/connect.js
+++ src/connect.js
@@ -1,5 +1,19 @@
+const AWS = require("aws-sdk");
+
 module.exports.handler = (event, context, callback) => {
+  const docclient = new AWS.DynamoDB.DocumentClient({
+    apiVersion: "2012-08-10",
+  });
+  const { CONNECTION_TABLE } = process.env;
   const { connectionId } = event.requestContext;
+
+  // docclient.put({
+  //   TableName: CONNECTION_TABLE,
+  //   Item: {
+  //     connectionId,
+  //   },
+  // });
+
   console.log(event, context);
   callback(null, { statusCode: 200 });
 };
