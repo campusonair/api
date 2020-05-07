2ffade4a394febc84e431f35915527f643052b1d
naoki <n.globe.us@gmail.com>
2020-05-08 00:26:02 +0900

    Addd typescript config. serverless-offline v6~ dosenot have hot-reloading by bug, so added patch

--- src/lives/listLive.ts
+++ src/lives/listLive.ts
@@ -1,5 +1,4 @@
 module.exports.handler = async (event, context, callback) => {
-  console.log("hoihoihoi")
   return callback(null,
     {
       statusCode: 200,
@@ -7,6 +6,6 @@ module.exports.handler = async (event, context, callback) => {
         'Content-Type': 'text/html',
         'Access-Control-Allow-Origin': '*',
       },
-      body: "<h1>7777777777777777777777</h1>"
+      body: "<h1>helllo</h1>"
     });
 };
