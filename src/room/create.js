424bdf26df6f15903c17c938f01aa4a262ca6626
naoki <n.globe.us@gmail.com>
2020-05-23 15:12:35 +0900

    fix some

--- src/room/create.js
+++ src/room/create.js
@@ -4,6 +4,13 @@ module.exports.handler = (event, context, callback) => {
 
   console.log(event)
 
+  const body = JSON.parse(event.body)
+  const peerId = body.peerId
+  console.log(peerId)
+
+  const idToken = body.idToken
+  console.log(idToken)
+
   callback(null, {
     statusCode: 200,
     headers: {
